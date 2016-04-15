c534b66154bd45a96aed096cff49c0809ca4b57c
lanou3g <975157026@qq.com>
2016-04-15 16:21:54 +0800

    This is fistAction

---
+++ teamProject2/teamProject2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teamProject2
+//
+//  Created by lanou3g on 16/4/15.
+//  Copyright © 2016年 oceanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ teamProject2/teamProject2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  teamProject2
+//
+//  Created by lanou3g on 16/4/15.
+//  Copyright © 2016年 oceanli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
